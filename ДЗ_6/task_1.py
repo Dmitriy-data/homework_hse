56f7e7bb07f889dd064597a7c5727a80ffb280f9
Dmitriy-data <dmitriy.moiseev.2003@bk.ru>
2026-01-17 15:18:12 +0300

    Add quick_sort.py

---
+++ ДЗ_6/task_1.py
@@ -0,0 +1,21 @@
+def merge_sort(a):
+    a = list(a)
+    if len(a) <= 1:
+        return a
+    mid = len(a) // 2
+    left = merge_sort(a[:mid])
+    right = merge_sort(a[mid:])
+    out = []
+    i = j = 0
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            out.append(left[i]); i += 1
+        else:
+            out.append(right[j]); j += 1
+    out.extend(left[i:]); out.extend(right[j:])
+    return out
+
+
+if __name__ == "__main__":
+    nums = list(map(int, input("Enter integers: ").split()))
+    print("Sorted:", merge_sort(nums))
